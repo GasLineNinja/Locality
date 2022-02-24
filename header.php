153d1491fa05f904bfe830e79c0a18532b900b8e
Joseph Lundell <okisan.oa@gmail.com>
2022-02-23 21:48:31 -0600

    Fix

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+    <!-- Importing Style Sheet -->
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<!-- header stuff here -->
+</body>
+</html>
