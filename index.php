9465f5226cf46de81c00118503c2be1e73bf122e
Michael Strand <mdstrand@uwm.edu>
2022-02-25 15:22:03 -0600

    initial page layout, working out kinks

--- index.php
+++ index.php
@@ -21,3 +21,32 @@
 	//Include Footer
 	include('footer.php');
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Locality Index</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="stylelayout.css">
+</head>
+<body>
+
+<div class="row">
+  <div class="column side">
+	<img src="LogoSmall_3.0.png" width="25%">
+  </div>
+  
+  <div class="column middle">
+    <h2>Main Content</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+  </div>
+  
+  <div class="column side">
+	<img src="LogoSmall_3.0.png" width="25%">
+  </div>
+</div>
+  
+</body>
+</html>
