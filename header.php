0a2e2d7e85b7525cc1e074fb1a7806f387b6fbc4
Joseph Lundell <okisan.oa@gmail.com>
2022-03-10 16:53:38 -0600

    Turned the autofill for the search bar off

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
         <a style="margin-left:10px" href="login.php" class ="login">Login</a>
         <a style="margin-left:-20px" href="signup.php" class ="signup">Sign Up</a>
         <div class="search-container">
-          <form>
+          <form autocomplete="off">
 						<button class="search_btn" type="submit">Search</button>
             <input class="main_search"  type="text" placeholder="Search.." name="search">
           </form>
