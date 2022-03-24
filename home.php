1f71ab880c349d8c9447bd90e7ff7d875054f33a
Joseph <aemailfortrash@gmail.com>
2022-03-23 22:33:25 -0500

    Just centered map and temp section for acInfo

--- home.php
+++ home.php
@@ -44,9 +44,20 @@ if (!isset($_SESSION['agent']) OR ($_SESSION['agent'] != md5($_SERVER['HTTP_USER
   </div>
   
   <div class="column middle">
-    <div class="pull_content">
+
+  <div class="pull_content">
+      <p class="review_busName">ACCOUNT INFOMATION WOULD GO HERE</p>
+
+    
+  </div>
+  </br>
+
+
+  <div class="pull_content">
+    <center>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3330.4601443532665!2d-79.13427753120769!3d33.41124595587085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x890031609db58a79%3A0x4c1928338e07d867!2s617%20Springs%20Ave%2C%20Pawleys%20Island%2C%20SC%2029585!5e0!3m2!1sen!2sus!4v1648084354017!5m2!1sen!2sus" 
-      width="560" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+      width="560" height="450" style="border: 0;" allowfullscreen="" loading="lazy"></iframe>
+    </center>
     </div>
   </div>
   
